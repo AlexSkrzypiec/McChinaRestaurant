cb4082151838fc9161cb8fb3c78ec9b2fb8590dc
Alex Skrzypiec <alex_@DESKTOP-B9FOF0O>
2018-08-30 21:49:29 -0300

    Mais tela

--- TelaInicial/ViewWPF/Views/Janela_1.xaml.cs
+++ TelaInicial/ViewWPF/Views/Janela_1.xaml.cs
@@ -23,5 +23,20 @@ namespace ViewWPF.Views
         {
             InitializeComponent();
         }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Ham1.IsChecked = this.Ham2.IsChecked = this.Ham3.IsChecked = this.Ham4.IsChecked = this.Ham5.IsChecked =
+                this.Frie1.IsChecked = this.Frie2.IsChecked = this.Frie3.IsChecked = this.Frie4.IsChecked = this.Frie5.IsChecked =
+                    this.Drin1.IsChecked = this.Drin2.IsChecked = this.Drin3.IsChecked = this.Drin4.IsChecked = this.Drin5.IsChecked =
+                        this.deser1.IsChecked = this.deser2.IsChecked = this.deser3.IsChecked = this.deser4.IsChecked = this.deser5.IsChecked = false;
+
+            NomeCampo.Clear();
+            DataCampo.Clear();
+
+
+        }
+
+      
     }
 }
