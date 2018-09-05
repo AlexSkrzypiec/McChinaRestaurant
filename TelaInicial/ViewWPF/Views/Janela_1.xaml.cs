bdfeedc2ba228fc9e07baaf0e102a9b9daf2f881
Alex Skrzypiec <alex_@DESKTOP-B9FOF0O>
2018-09-04 22:02:22 -0300

    Mudanças do dia 04/09/2018

--- TelaInicial/ViewWPF/Views/Janela_1.xaml.cs
+++ TelaInicial/ViewWPF/Views/Janela_1.xaml.cs
@@ -29,7 +29,8 @@ namespace ViewWPF.Views
             this.Ham1.IsChecked = this.Ham2.IsChecked = this.Ham3.IsChecked = this.Ham4.IsChecked = this.Ham5.IsChecked =
                 this.Frie1.IsChecked = this.Frie2.IsChecked = this.Frie3.IsChecked = this.Frie4.IsChecked = this.Frie5.IsChecked =
                     this.Drin1.IsChecked = this.Drin2.IsChecked = this.Drin3.IsChecked = this.Drin4.IsChecked = this.Drin5.IsChecked =
-                        this.deser1.IsChecked = this.deser2.IsChecked = this.deser3.IsChecked = this.deser4.IsChecked = this.deser5.IsChecked = false;
+                        this.deser1.IsChecked = this.deser2.IsChecked = this.deser3.IsChecked = this.deser4.IsChecked = this.deser5.IsChecked =
+                            this.food1.IsChecked = this.food2.IsChecked = this.food3.IsChecked = this.food4.IsChecked = this.food5.IsChecked = false;
 
             NomeCampo.Clear();
             DataCampo.Clear();
@@ -37,6 +38,11 @@ namespace ViewWPF.Views
 
         }
 
-      
+        private void SairButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.Close();
+
+        }
     }
 }
